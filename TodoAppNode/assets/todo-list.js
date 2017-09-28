7e49275aba923b5537ca78578b5eed496a9ad9b4
BlackIce21 <blackice9107@gmail.com>
2017-09-28 21:35:51 +0530

    complete v1.0 of todo app

--- TodoAppNode/assets/todo-list.js
+++ TodoAppNode/assets/todo-list.js
@@ -1,37 +1,36 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-  $('form').on('submit', function(){
+  $('form').on('submit', function() {
 
-      var item = $('form input');
-      var todo = {item: item.val()};
+    var item = $('form input');
+    var todo = {
+      item: item.val()
+    };
 
-      $.ajax({
-        type: 'POST',
-        url: '/todo',
-        data: todo,
-        success: function(data){
-          //do something with the data via front-end framework
-          location.reload();
-        }
-      });
+    $.ajax({
+      type: 'POST',
+      url: '/todo',
+      data: todo,
+      success: function(data) {
+        //do something with the data via front-end framework
+        location.reload();
+      }
+    });
 
-      $('form').trigger('reset');
-      return false;
-
-
-
+    $('form').trigger('reset');
+    return false;
   });
 
-  $('li').on('click', function(){
-      var item = $(this).text().replace(/ /g, "-");
-      $.ajax({
-        type: 'DELETE',
-        url: '/todo/' + item,
-        success: function(data){
-          //do something with the data via front-end framework
-          location.reload();
-        }
-      });
+  $('li').on('click', function() {
+    var item = $(this).text().replace(/ /g, "-");
+    $.ajax({
+      type: 'DELETE',
+      url: '/todo/' + item,
+      success: function(data) {
+        //do something with the data via front-end framework
+        location.reload();
+      }
+    });
   });
 
 });
--- TodoAppNode/controllers/todoController.js
+++ TodoAppNode/controllers/todoController.js
@@ -1,31 +1,57 @@
-var data = [{
-  item: 'take dog'
-}, {
-  item: 'buy veggies'
-}, {
-  item: 'buy shoes'
-}];
+// var data = [{
+//   item: 'take dog'
+// }, {
+//   item: 'buy veggies'
+// }, {
+//   item: 'buy shoes'
+// }];
+// Parsing POST data
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({
   extended: false
 });
+var mongoose = require('mongoose');
+
+// Connecting to the database
+mongoose.connect('mongodb://testme:test@ds151554.mlab.com:51554/todotest')
+
+// Creating schema
+var todoSchema = new mongoose.Schema({
+  item: String
+});
+// Creating model based on schema
+var Todo = mongoose.model('todo', todoSchema);
+//Adding item to collection
+// var item1 = Todo({item: 'Buy dog food'}).save(function(err){
+//   if (err) throw err;
+//   console.log("Item Saved.");
+// });
+
 module.exports = function(app) {
 
   app.get('/todo', (req, res) => {
-    res.render('todo', {test: data});
+    Todo.find({}, function(err, data) { // Getting data from DB and render the view
+      if (err) throw err;
+      res.render('todo', {
+        test: data
+      });
+
+    });
   });
 
   app.post('/todo', urlencodedParser, (req, res) => {
-    data.push(req.body);
-    res.render('todo', {test: data});
-  });
+    var newTodo = Todo(req.body).save(function(err) { // Adding data to DB from form using req.body
+      if (err) throw err;
+      res.render('todo', {
+        test: newTodo
+      });
+    });
+});
 
   app.delete('/todo/:item', (req, res) => {
-    data = data.filter(function(todo) {
-      return todo.item.replace(/ /g, '-') !== req.params.item;
+    Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove(function(err, data){
+      if (err) throw err;
+      res.json(data);
     });
-    res.json(data);
   });
-
-
 };
